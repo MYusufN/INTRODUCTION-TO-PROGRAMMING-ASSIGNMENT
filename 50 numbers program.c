29de93e867ae2320739d8bf6ddaeb37d8bc8706f
MYusufN <101639421+MYusufN@users.noreply.github.com>
2022-04-19 10:19:20 +0300

    Add files via upload

---
+++ 50 numbers program.c
@@ -0,0 +1,12 @@
+//program to display the first 50 natural numbers in reverse order
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("The first 50 numbers using for loop. \n");
+	for(num=50; num>=1; num--)
+	{
+		printf("%d\n",num);
+	}
+	//printf("\n");
+}
